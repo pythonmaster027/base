641f905ab7ce77a4cfbe60a5e78eae8d3d7b671a
pythonmaster027 <121269619+pythonmaster027@users.noreply.github.com>
2022-12-23 02:50:41 +0000

    commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
+const mcdonalds = {storeName: "McDonalds", location:"100 Hancock St",deliveryFee:2,con:{a:"A",b:"B",c:"C",d:"D"}};
+const burgerKing = {storeName: "Burger King", location:"10 Granite St", deliveryFee:2,con:{a:"A",b:"B",c:"C",d:"D"}};
+const wendys = {storeName: "Wendys", location:"50 Newport Ave", deliveryFee:2,con:{a:"A",b:"B",c:"C",d:"D"}};
+const popeyes = {storeName: "Popeyes", location:"680 Southern Artery", deliveryFee:2,con:{a:"A",b:"B",c:"C",d:"D"}};
+const subway = {storeName: "Subway", location:"1800 Morrisey Blvd", deliveryFee:2};
+const kfc = {storeName: "KFC", location:"500 Hancock St", deliveryFee:2};
+const dunkin = {storeName: "Dunkin", location:"50 Hancock St", deliveryFee:2};
+const panera = {storeName: "Panera", location:"200 Hancock St", deliveryFee:2};
+
+
+const main = [mcdonalds,burgerKing,wendys,popeyes,subway,kfc,dunkin,panera];
+
 
